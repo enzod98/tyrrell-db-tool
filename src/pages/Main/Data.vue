aac25d9babc313962e626c6bbfd38f0a47dc32ae
edure <edure@ecop.com.py>
2023-03-24 17:01:38 -0300

    avances en la pestaña Data

--- src/pages/Main/Data.vue
+++ src/pages/Main/Data.vue
@@ -1,6 +1,31 @@
 <template>
-  <q-page class="bg-grey-4">
-    <h2 class="text-center text-red">ACÁ HAY DATOS</h2>
+  <q-page class="container">
+    <div class="row">
+      <div class="q-pa-md col-4">
+        <q-card bordered>
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="~assets/images/main-page/manager.png" />
+              </q-avatar>
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Manager</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-separator />
+          <q-card-section horizontal>
+            <q-card-section>
+              <q-table />
+            </q-card-section>
+            <q-separator vertical />
+            <q-card-section class="col-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </q-card-section>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
